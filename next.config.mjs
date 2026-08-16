12b49a009079b20a274f3b39bd2d884be5fbe8dc
Olodude Okikiola <olodudeokiki@gmail.com>
2026-08-16 18:08:51 +0100

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config, { isServer }) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: false,
+        fs: false,
+        path: false,
+      };
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
